b9d4bd32f6300dcc3e12eb650b0116a3b2312816
tjvandal <tvandal20@gmail.com>
2016-04-09 15:22:06 -0400

    lons for 0 to 360

--- scripts/trmm_to_netcdf.py
+++ scripts/trmm_to_netcdf.py
@@ -7,19 +7,20 @@ import datetime as dt
 file_dir = "/gss_gpfs_scratch/vandal.t/trmm/raw/"
 netcdf_dir = "/gss_gpfs_scratch/vandal.t/trmm/netcdf/"
 lats = np.linspace(-49.875, 49.875, 400)
-lons = np.linspace(-179.875, 179.875, 1440)
+lons = np.linspace(0.125, 359.875, 1440)
 
 prev_year = 1998
 data, dates = [], []
 
 for f in sorted(os.listdir(file_dir)):
     _, year, month, day, _, _ = f.split(".")
+    year = int(year)
     date = dt.datetime(int(year), int(month), int(day))
-    if (prev_year != int(year)):
+    if (prev_year != year):
         data = np.concatenate(data, axis=2)
         dr = xr.DataArray(data, coords=[lats, lons, dates], dims=['lat', 'lon', 'time'])
         ds = xr.Dataset(dict(prcp=dr))
-        ds.to_netcdf(os.path.join(netcdf_dir, "trmm_3B42_%s.nc" % year))
+        ds.to_netcdf(os.path.join(netcdf_dir, "trmm_3B42_%i.nc" % prev_year))
         prev_year = year
         data = []
         dates = []
